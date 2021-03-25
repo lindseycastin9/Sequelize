a698e694d2035bdd0e478500ff30c296300522b8
lindseycastin9 <78098727+lindseycastin9@users.noreply.github.com>
2021-03-25 16:07:44 -0400

    jj

--- public/script.js
+++ public/script.js
@@ -1,4 +1,48 @@
-async function windowActions() {
-    console.log('window loaded');
-    const request = await fetch('/api');
-    const data = await request.json()
+async function populateRestaurants() {
+    const diningRequest = await fetch('/api/meals');
+    const diningData = await diningRequest.json();
+
+    diningData.data.forEach((restaurant) => {
+        const appendItem = document.createElement('div');
+        appendItem.classList.add('title', 'has-text-centered', 'is=parent', 'is-3');
+        appendItem.innerHTML = `
+        <article class="title is-child box has-background-light-dark ">
+        <span class= "subtitle has-text-light has-text-weight-bold>
+            ${restaurant.hall_name}</span>
+        <br />
+        <span class = "has-text-light">
+            ${restaurant.hall_address.split(',')[0]}
+        </span>
+        <br/>
+        <span class="has-text-light">
+            ${restaurant.hall_address.split(',')[1]}
+        </span>    
+        </article `;
+    targetBox.append(appendItem);
+
+    });
+
+}
+
+async function getData() {
+    console.log('data request');
+    const diningRequest = await fetch('/api/meals');
+    const diningData = await diningRequest.json();
+    return diningData
+}
+
+async function setBasicData() {
+    localStorage.setItem('abd' , 'zyx');
+}
+
+async function getBasicData () {
+    const cat = localStorage.getItem('mycat');
+}
+
+async function windowActions () {
+    console.log('loaded window');
+    const data = await getData();
+    console.table(data)
+}
+
+window.onload = windowActions; 
