3f5430d6edbd390d9e43cb811768e8bb01bd2aad
lindseycastin9 <78098727+lindseycastin9@users.noreply.github.com>
2021-03-25 21:47:42 -0400

    wed

--- public/script.js
+++ public/script.js
@@ -1,58 +1,30 @@
-async function populateRestaurants() {
-    const diningRequest = await fetch('/api/meals');
-    const diningData = await diningRequest.json();
+async function getData() {
+    const endpoint = '/api/dining/'; 
+    const request = await fetch(endpoint);
+    const result = await request.json();
+  
 
-    diningData.data.forEach((restaurant) => {
-        const appendItem = document.createElement('div');
-        appendItem.classList.add('title', 'has-text-centered', 'is=parent', 'is-3');
-        appendItem.innerHTML = `
-        <article class="title is-child box has-background-light-dark ">
-        <span class= "subtitle has-text-light has-text-weight-bold>
-            ${restaurant.hall_name}</span>
-        <br />
-        <span class = "has-text-light">
-            ${restaurant.hall_address.split(',')[0]}
-        </span>
-        <br/>
-        <span class="has-text-light">
-            ${restaurant.hall_address.split(',')[1]}
-        </span>    
-        </article `;
-    targetBox.append(appendItem);
-
-    });
-
-}
-
-async function getMeals() {
-    console.log('data request');
-    const diningRequest = await fetch('/api/meals');
-    const diningData = await diningRequest.json();
-    return diningData
-}
-
-async function setBasicData() {
-    localStorage.setItem('abd' , 'zyx');
-}
-
-async function getBasicData () {
-    return localStorage.getItem('mycat');
-}
-
-async function setComplexData(data) {
-    localStorage.setItem('data', JSON.stringify(data));
-}
-
-async function windowActions () {
-    console.log('loaded window');
-    const meals = await getMeals();
-    console.table(meals);
-    setComplexData(meals);
-    const storedMeals = localStorage.getItem('data');
-    const storedMealsData = JSON.parse(storedMeals);
-    console.log(storedMeals);
-    console.log(storedMealsData);
+    const meals = [];
+    meals.push(result.data);
+  
+    const finalMeals = meals[0]; 
+    console.table(finalMeals);
+    const target = document.querySelector('#finalTable');
     
-}
-
-window.onload = windowActions; 
+    finalMeals.forEach((index) => {
+      const id = index.hall_id;
+      const name = index.hall_name;
+      const location = index.hall_address;
+  
+      const appendItem = document.createElement('tr');
+      appendItem .innerHTML = `<td>${id}</td> <td>${name}</td> <td>${location}</td>`;
+      target.append(appendItem);
+      console.log('ID: ' + id + ', Name: ' + name + ', Location: ' + location);
+    }); 
+  }
+  
+  async function windowActions() {
+    getData();
+  }
+    
+  window.onload = windowActions; 
