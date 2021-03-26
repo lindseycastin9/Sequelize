d3066026914b97ea7e5b7856d160e01e0134a25c
lindseycastin9 <78098727+lindseycastin9@users.noreply.github.com>
2021-03-25 22:02:24 -0400

    ljk

--- public/script.js
+++ public/script.js
@@ -4,22 +4,24 @@ async function getData() {
     const result = await request.json();
   
 
-    const meals = [];
-    meals.push(result.data);
+    const diner = [];
+    diner.push(result.data);
   
-    const finalMeals = meals[0]; 
-    console.table(finalMeals);
+    const DiningHallData = diner[0]; 
+    console.table(DiningHallData);
     const target = document.querySelector('#finalTable');
+
     
-    finalMeals.forEach((index) => {
-      const id = index.hall_id;
-      const name = index.hall_name;
-      const location = index.hall_address;
+    
+    DiningHallData.forEach((item) => {
+      const id = item.hall_id;
+      const name = item.hall_name;
+      const address = item.hall_address;
   
       const appendItem = document.createElement('tr');
-      appendItem .innerHTML = `<td>${id}</td> <td>${name}</td> <td>${location}</td>`;
+      appendItem .innerHTML = `<td>${id}</td> <td>${name}</td> <td>${address}</td>`;
       target.append(appendItem);
-      console.log('ID: ' + id + ', Name: ' + name + ', Location: ' + location);
+      console.log('ID: ' + id + ', Name: ' + name + ', Location: ' + address);
     }); 
   }
   
